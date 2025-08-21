5ef566eb2502279ba2cc89df891a9f092877ac26
Shaun Underwood <su@sunderwood.co.uk>
2025-08-21 23:09:38 +0100

    Add LogoAndSkip component to onboarding screen with skip functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="mobile-container backgound-image">
     <ProgressBar :steps="7" :currentStep="2" />
-
+    <LogoAndSkip @onClick="onSkipClick" />
   </div>
 </template>
 
 <script setup>
-//
+import LogoAndSkip from '@/components/LogoAndSkip.vue';
+
+const onSkipClick = () => {
+  alert('skip');
+}
+
 </script>
 
 <style>
@@ -16,6 +21,4 @@
   background-position: center;
    position: relative; 
 }
-
-
 </style>
