23a276f727112b1385197d9679f87d6ee846e5c4
Shaun Underwood <su@sunderwood.co.uk>
2025-08-21 23:09:08 +0100

    LogoAndSkip component

---
+++ components/LogoAndSkip.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logo-and-skip">
+    <img class="logo-and-skip__logo" src="/public/logo.svg" alt="Logo" />
+    <UButton class="logo-and-skip__button" @click="onClick">
+      Skip
+    </UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['onClick']);
+const onClick = () => emit('onClick');
+</script>
+
+<style scoped>
+.logo-and-skip {
+  @apply flex items-center justify-between px-4 pt-6 pb-6;
+
+  &__button {
+    /* TODO: bring transition out to cover all buttons */
+    @apply bg-black/50 text-white font-medium text-[17px] px-2 py-1 rounded-full transition-all duration-200;
+  }
+
+  &__button:hover {
+    @apply bg-brand-aqua;
+  }
+}
+</style>
