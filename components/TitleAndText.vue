f26a065bc70a5879783dd4f3f1e0c170a8acde01
Shaun Underwood <su@sunderwood.co.uk>
2025-08-21 23:34:00 +0100

    Add TitleAndText component

---
+++ components/TitleAndText.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="title-and-text">
+    <h2 class="title-and-text__title">
+      {{ title.split('\n').join('\n') }}
+    </h2>
+
+    <p class="title-and-text__description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  description: string;
+}>();
+</script>
+<style scoped>
+.title-and-text {
+  @apply flex flex-col gap-3 px-4;
+
+  &__title {
+    @apply text-white text-[34px] font-bold leading-[41px] tracking-[-0.4px];
+  }
+
+  &__description {
+    @apply text-white text-[17px] font-normal leading-[22px] tracking-[-0.43px] opacity-90;
+  }
+}
+</style>
