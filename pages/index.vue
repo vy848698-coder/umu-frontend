d9ecb929b60fa9c18347ad330afc4114c2a7c4a7
Shaun Underwood <su@sunderwood.co.uk>
2025-08-21 23:33:48 +0100

    darken the background more as descriptive text hard to read

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@ const onSkipClick = () => {
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: rgba(0, 0, 0, 0.6);
   pointer-events: none;
   z-index: 1;
 }
