5e29e72f163111d630ab470fc537e9bff2a4b387
Shaun Underwood <su@sunderwood.co.uk>
2025-08-21 21:48:43 +0100

    ProgressBar component

---
+++ components/ProgressBar.vue
@@ -0,0 +1,35 @@
+<!-- ProgressBar.vue -->
+<template>
+  <div class="progress">
+    <div 
+      v-for="(segment, index) in segments" 
+      :key="index"
+      class="progress__segment"
+      :class="{ 'progress__segment--filled': segment.filled }"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  steps: number;
+  currentStep: number;
+}>();
+const segments = Array.from({ length: props.steps }, (_, index) => ({
+  filled: index < props.currentStep,
+}));
+</script>
+
+<style scoped>
+.progress {
+  @apply flex items-center justify-center gap-1 px-6 mt-3;
+}
+
+.progress__segment {
+  @apply h-1 bg-white/20 rounded-full overflow-hidden flex-1;
+}
+
+.progress__segment--filled {
+  @apply bg-white;
+}
+</style>
